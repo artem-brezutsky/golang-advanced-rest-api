7566ccdf19ad993da15c2e0fa0b394b38a1e6fb4
artem-brezutsky <a.brezutsky@gmail.com>
2022-04-28 23:40:32 +0300

    commit

--- cmd/internal/user/handler.go
+++ cmd/internal/user/handler.go
@@ -1,1 +1,49 @@
 package user
+
+import (
+	"advanced-rest-api/cmd/internal/handlers"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+const (
+	usersUrl = "/users"
+	userUrl  = "/users/:uuid"
+)
+
+var _ handlers.Handler = &handler{}
+
+type handler struct {
+}
+
+func NewHandler() handlers.Handler {
+	return &handler{}
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersUrl, h.GetList)
+	router.GET(userUrl, h.GetUserByUUID)
+	router.POST(usersUrl, h.CreateUser)
+	router.PUT(userUrl, h.UpdateUser)
+	router.PATCH(userUrl, h.PartialUpdateUser)
+	router.DELETE(userUrl, h.DeleteUser)
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is list of users"))
+}
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is create user"))
+}
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is user by uiid"))
+}
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this update user"))
+}
+func (h *handler) PartialUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is partial update user"))
+}
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is delete user"))
+}
--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"advanced-rest-api/cmd/internal/user"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net"
@@ -9,17 +9,18 @@ import (
 	"time"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	name := params.ByName("name")
-
-	w.Write([]byte(fmt.Sprintf("Hello %s", name)))
-}
-
 func main() {
+	log.Println("create logger")
 	router := httprouter.New()
 
-	router.GET("/:name", IndexHandler)
+	log.Println("register user handler")
+	handler := user.NewHandler()
+	handler.Register(router)
 
+	start(router)
+}
+
+func start(router *httprouter.Router) {
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		panic(err)
