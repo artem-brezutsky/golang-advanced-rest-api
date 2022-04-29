81f3e344899a2372b3b68fc6b0de11ed1d802a81
artem-brezutsky <a.brezutsky@gmail.com>
2022-04-29 22:31:30 +0300

    commit

--- cmd/internal/user/handler.go
+++ cmd/internal/user/handler.go
@@ -22,28 +22,34 @@ func NewHandler() handlers.Handler {
 
 func (h *handler) Register(router *httprouter.Router) {
 	router.GET(usersUrl, h.GetList)
-	router.GET(userUrl, h.GetUserByUUID)
 	router.POST(usersUrl, h.CreateUser)
+	router.GET(userUrl, h.GetUserByUUID)
 	router.PUT(userUrl, h.UpdateUser)
 	router.PATCH(userUrl, h.PartialUpdateUser)
 	router.DELETE(userUrl, h.DeleteUser)
 }
 
 func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
 	w.Write([]byte("this is list of users"))
 }
 func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
 	w.Write([]byte("this is create user"))
 }
 func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
 	w.Write([]byte("this is user by uiid"))
 }
 func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("this update user"))
 }
 func (h *handler) PartialUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("this is partial update user"))
 }
 func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("this is delete user"))
 }
