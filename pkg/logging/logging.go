570faa896a6cd30f299565f1219c91946c646964
artem-brezutsky <a.brezutsky@gmail.com>
2022-04-29 23:29:09 +0300

    commit

---
+++ pkg/logging/logging.go
@@ -0,0 +1,51 @@
+package logging
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"path"
+	"runtime"
+)
+
+type writerHook struct {
+	Writer    []io.Writer
+	LogLevels []logrus.Level
+}
+
+func (hook *writerHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+
+	for _, w := range hook.Writer {
+		w.Write([]byte(line))
+	}
+	return err
+}
+
+func Init() {
+	l := logrus.New()
+	l.SetReportCaller(true)
+	l.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			filename := path.Base(frame.File)
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
+		},
+		//DisableColors: false,
+		FullTimestamp: true,
+	}
+
+	err := os.MkdirAll("logs", 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	allFile, err := os.OpenFile("logs/all.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+}
